d235cf02521d98897689cf9f4fee41004e081599
ProjectCleverWeb <Administrator@ProjectCleverWeb.com>
2014-02-20 19:52:41 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,4 +1,72 @@
-Phpuri
-=========
+#PHP Parse URI
 
-To be replaced
+PHP library for working with URI's. Requires PHP `5.3.7` or later. Replaces and extends PHP's `parse_url()`.
+
+
+
+Copyright &copy; 2014 Nicholas Jordon - All Rights Reserved <br>
+Licensed under the MIT license
+
+---
+
+##Usage
+
+####Example #1: <small>String Operations</small>
+
+```php
+<?php
+$uri = new parse_uri('http://example.com/path/to/file.ext');
+
+$uri->replace('QUERY', array('rand', (string) rand(1, 10)));
+$uri->replace('PATH', '/foo/bar');
+$uri->append('PATH', '.baz');
+$new = $uri->prepend('HOST', 'www.');
+
+$uri->reset();
+$original = $uri->str();
+
+$uri->replace('FRAGMENT', 'Checkout')
+$secure = $uri->replace('SCHEME', 'https');
+
+echo $new.PHP_EOL;
+echo $original.PHP_EOL;
+echo $secure.PHP_EOL;
+```
+
+**Output:**
+```
+http://www.example.com/foo/bar.baz?rand=2
+http://example.com/path/to/file.ext
+https://example.com/path/to/file.ext#Checkout
+```
+
+
+####Example #2: <small>Information Gathering</small>
+
+```php
+<?php
+$uri = new parse_uri('http://example.com/path/to/file.ext?q=1');
+
+if ($uri->scheme_name == 'https') {
+	echo 'Uses SSL';
+} else {
+	echo 'Does not use SSL';
+}
+
+$link = sprintf('<a href="%1$s">%2$s</a>', $uri->str(), $uri->host.$uri->path);
+
+$uri = new parse_uri('ftp://jdoe@example.com/my/home/dir');
+$login = array(
+	'username' => $uri->user,
+	'password' => $user_input,
+	'domain'   => $uri->host,
+	'path'     => $uri->path
+);
+
+```
+
+**Output:**
+```
+Does not use SSL
+```
+
--- test.php
+++ test.php
@@ -1,95 +1,14 @@
 <?php
 /**
- * WARNING
- * 
- * THIS SCRIPT DOES NOT CURRENTLY WORK
+ * Evaluate the script functions as expected
  */
 
+require __DIR__.'./parse_uri.php';
 
-require('../rel2abs.php');
-require('../url_to_absolute.php');
-require('phpuri.php');
+$uri1 = new parse_uri('http://jdoe:test123@example.com:700/path/to/file.ext?q=1#frag');
+$uri2 = new parse_uri('//google.com');
+$uri3 = new parse_uri('example.com/foo');
 
-$tests=array(
-  array('rel' => 'g:h', 'result' => 'g:h'),
-  array('rel' => 'g',   'result' => 'http://a/b/c/g'),
-  array('rel' => './g', 'result' => 'http://a/b/c/g'),
-  array('rel' => 'g/',  'result' => 'http://a/b/c/g/'),
-  array('rel' => '/g',  'result' => 'http://a/g'),
-  array('rel' => '//g', 'result' => 'http://g'),
-  array('rel' => 'g?y', 'result' => 'http://a/b/c/g?y'),
-  array('rel' => '#s', 'result' => 'http://a/b/c/d;p?q#s'),
-  array('rel' => 'g#s', 'result' => 'http://a/b/c/g#s'),
-  array('rel' => 'g?y#s', 'result' => 'http://a/b/c/g?y#s'),
-  array('rel' => ';x', 'result' => 'http://a/b/c/;x'),
-  array('rel' => 'g;x', 'result' => 'http://a/b/c/g;x'),
-  array('rel' => 'g;x?y#s', 'result' => 'http://a/b/c/g;x?y#s'),
-  array('rel' => '.', 'result' => 'http://a/b/c/'),
-  array('rel' => './', 'result' => 'http://a/b/c/'),
-  array('rel' => '..', 'result' => 'http://a/b/'),
-  array('rel' => '../', 'result' => 'http://a/b/'),
-  array('rel' => '../g', 'result' => 'http://a/b/g'),
-  array('rel' => '../..', 'result' => 'http://a/'),
-  array('rel' => '../../', 'result' => 'http://a/'),
-  array('rel' =>'../../g','result' =>'http://a/g'),
-  array('rel' =>'g.','result' =>'http://a/b/c/g.'),
-  array('rel' =>'.g','result' =>'http://a/b/c/.g'),
-  array('rel' =>'g..','result' =>'http://a/b/c/g..'),
-  array('rel' =>'..g','result' =>'http://a/b/c/..g'),
-  array('rel' =>'./../g','result' =>'http://a/b/g'),
-  array('rel' =>'./g/.','result' =>'http://a/b/c/g/'),
-  array('rel' =>'g/./h','result' =>'http://a/b/c/g/h'),
-  array('rel' =>'g/../h','result' =>'http://a/b/c/h'),
-  array('rel' =>'g;x=1/./y','result' =>'http://a/b/c/g;x=1/y'),
-  array('rel' =>'g;x=1/../y','result' =>'http://a/b/c/y'),
-  array('rel' =>'g?y/./x','result' =>'http://a/b/c/g?y/./x'),
-  array('rel' =>'g?y/../x','result' =>'http://a/b/c/g?y/../x'),
-  array('rel' =>'g#s/./x','result' =>'http://a/b/c/g#s/./x'),
-  array('rel' =>'g#s/../x','result' =>'http://a/b/c/g#s/../x')
-);
+print_r($uri2);
 
-# rel2abs
-$start = microtime();
-$base = 'http://a/b/c/d;p?q';
-list($successes, $failures) = array(0,0);
-foreach($tests as $test){
-  if(($r = rel2abs($test['rel'], $base)) == $test['result']){
-    $successes++;
-  } else {
-    $failures++;
-  }
-}
-
-$elapsed = microtime() - $start;
-echo "rel2abs:         successes -> $successes, failures => $failures, elapsed time: $elapsed\n";
-
-# url_to_absolute
-$start = microtime();
-$base = 'http://a/b/c/d;p?q';
-list($successes, $failures) = array(0,0);
-foreach($tests as $test){
-  if(($r = url_to_absolute($base, $test['rel'])) == $test['result']){
-    $successes++;
-  } else {
-    $failures++;
-  }
-}
-
-$elapsed = microtime() - $start;
-echo "url_to_absolute: successes -> $successes, failures => $failures, elapsed time: $elapsed\n";
-
-# phpuri
-$start = microtime();
-$base = phpUri::parse('http://a/b/c/d;p?q');
-list($successes, $failures) = array(0,0);
-foreach($tests as $test){
-  if(($r = $base->join($test['rel'])) == $test['result']){
-    $successes++;
-  } else {
-    $failures++;
-    echo "failure: $r instead of " . $test['result'] . " \n";
-  }
-}
-$elapsed = microtime() - $start;
-echo "phpuri:          successes -> $successes, failures => $failures, elapsed time: $elapsed\n";
 ?>
