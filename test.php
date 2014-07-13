8bbbc1db2d1d9d4da77e7456650a400f5dcf3334
ProjectCleverWeb <Administrator@ProjectCleverWeb.com>
2014-07-13 03:42:15 -0400

    improved PHPUnit tests

--- test.php
+++ test.php
@@ -1,21 +1,18 @@
 <?php
 
+require_once __DIR__.'/uri.class.php';
+
 /**
  * @requires PHP 5.3.7
  * @requires PHPUnit 4.1
  */
 class URITest extends PHPUnit_Framework_TestCase {
 	
-	public function setUp() {
-		$this->assertFileExists(__DIR__.'/uri.class.php');
-		require_once __DIR__.'/uri.class.php';
-	}
-	
-	public function testMinimalParsing() {
+	/**
+	 * @test
+	 */
+	public function Minimal_Parsing() {
 		$uri1 = new uri('example.com');
-		
-		// Is the RegExp valid?
-		$this->assertSame(0, preg_match(uri::PARSER_REGEX, null), 'Parser RegExp is invalid');
 		
 		// Check For Errors
 		$this->assertEmpty($uri1->error);
@@ -25,9 +22,10 @@ class URITest extends PHPUnit_Framework_TestCase {
 	}
 	
 	/**
-	 * @depends testMinimalParsing
+	 * @test
+	 * @depends Minimal_Parsing
 	 */
-	public function testSimpleParsing() {
+	public function Simple_Parsing() {
 		$uri1 = new uri('http://example.com/sample');
 		
 		// Check For Errors
@@ -41,10 +39,11 @@ class URITest extends PHPUnit_Framework_TestCase {
 	}
 	
 	/**
-	 * @depends testSimpleParsing
+	 * @test
+	 * @depends Simple_Parsing
 	 */
-	public function testAdvancedParsing() {
-		$uri1 = new uri('https://example.com:777/path/to/script.php?query=str#fragment');
+	public function Advanced_Parsing() {
+		$uri1 = new uri('https://user:pass@example.com:777/path/to/script.php?query=str#fragment');
 		
 		// Check For Errors
 		$this->assertEmpty($uri1->error);
@@ -53,6 +52,8 @@ class URITest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals('https://', $uri1->scheme);
 		$this->assertEquals('https', $uri1->scheme_name);
 		$this->assertEquals('://', $uri1->scheme_symbols);
+		$this->assertEquals('user', $uri1->user);
+		$this->assertEquals('pass', $uri1->pass);
 		$this->assertEquals('example.com', $uri1->host);
 		$this->assertEquals('777', $uri1->port);
 		$this->assertEquals('/path/to/script.php', $uri1->path);
@@ -61,10 +62,11 @@ class URITest extends PHPUnit_Framework_TestCase {
 	}
 	
 	/**
-	 * @depends testAdvancedParsing
+	 * @test
+	 * @depends Advanced_Parsing
 	 */
-	public function testSimpleOutput() {
-		$input = 'https://example.com:777/path/to/script.php?query=str#fragment';
+	public function Simple_Output() {
+		$input = 'https://user:pass@example.com:777/path/to/script.php?query=str#fragment';
 		$uri1 = new uri($input);
 		
 		// Check For Errors
@@ -78,9 +80,27 @@ class URITest extends PHPUnit_Framework_TestCase {
 	}
 	
 	/**
-	 * @depends testAdvancedParsing
+	 * @test
+	 * @depends Advanced_Parsing
 	 */
-	public function testSimpleReplace() {
+	public function Aliases() {
+		$uri1 = new uri('https://user:pass@example.com:777/path/to/script.php?query=str#fragment');
+		
+		// Check For Errors
+		$this->assertEmpty($uri1->error);
+		
+		// Check Output
+		$this->assertSame($uri1->scheme, $uri1->protocol);
+		$this->assertSame($uri1->user, $uri1->username);
+		$this->assertSame($uri1->pass, $uri1->password);
+		$this->assertSame($uri1->fqdn, $uri1->fqdn);
+	}
+	
+	/**
+	 * @test
+	 * @depends Advanced_Parsing
+	 */
+	public function Simple_Replace() {
 		$uri1 = new uri('example.com/original/path');
 		
 		// Check For Errors
@@ -91,11 +111,13 @@ class URITest extends PHPUnit_Framework_TestCase {
 	}
 	
 	/**
-	 * @depends testAdvancedParsing
-	 * @depends testSimpleReplace
-	 * @depends testSimpleOutput
+	 * @test
+	 * @depends Advanced_Parsing
+	 * @depends Simple_Replace
+	 * @depends Simple_Output
+	 * @depends Aliases
 	 */
-	public function testReset() {
+	public function Reset() {
 		$uri1 = new uri('example.com/original/path');
 		
 		// Check For Errors
@@ -108,40 +130,348 @@ class URITest extends PHPUnit_Framework_TestCase {
 	}
 	
 	/**
-	 * @depends testReset
+	 * @test
+	 * @depends Reset
+	 * @covers uri::_modifier
+	 * @covers uri::replace
+	 * @covers uri::prepend
+	 * @covers uri::append
 	 */
-	public function testReplace() {
+	public function Modify_Scheme_Name() {
+		// Test both when there is and isn't pre-existing data
 		$uri1 = new uri('example.com');
-		$uri2 = new uri('https://example.com:777/path/to/script.php?query=str#fragment');
+		$uri2 = new uri('https://user:pass@example.com:777/path/to/script.php?query=str#fragment');
 		
 		// Check For Errors
 		$this->assertEmpty($uri1->error);
 		$this->assertEmpty($uri2->error);
 		
 		// Check Replace
-		/*** Scheme Name ***/
 		$this->assertEquals(
-			'https://example.com',
-			$uri1->replace('SCHEME_NAME', 'https')
+			'ftp://example.com',
+			$uri1->replace('SCHEME_NAME', 'ftp')
 		);
 		$this->assertEquals(
-			'http://example.com:777/path/to/script.php?query=str#fragment',
-			$uri2->replace('SCHEME_NAME', 'http')
+			'ftp://user:pass@example.com:777/path/to/script.php?query=str#fragment',
+			$uri2->replace('SCHEME_NAME', 'ftp')
 		);
-		$uri1->reset();
-		$uri2->reset();
-		/*** Scheme Symbols ***/
+		// Check Prepend
+		$this->assertEquals(
+			'sftp://example.com',
+			$uri1->prepend('SCHEME_NAME', 's')
+		);
+		// Check Append
+		$this->assertEquals(
+			'ftpes://user:pass@example.com:777/path/to/script.php?query=str#fragment',
+			$uri2->append('SCHEME_NAME', 'es')
+		);
+	}
+	
+	/**
+	 * @test
+	 * @depends Reset
+	 * @covers uri::_modifier
+	 * @covers uri::replace
+	 * @covers uri::prepend
+	 * @covers uri::append
+	 */
+	public function Modify_Scheme_Symbols() {
+		// Test both when there is and isn't pre-existing data
+		$uri1 = new uri('example.com');
+		$uri2 = new uri('https://user:pass@example.com:777/path/to/script.php?query=str#fragment');
+		
+		// Check For Errors
+		$this->assertEmpty($uri1->error);
+		$this->assertEmpty($uri2->error);
+		
+		// Check Replace
 		$this->assertEquals(
 			'//example.com',
 			$uri1->replace('SCHEME_SYMBOLS', '//')
 		);
 		$this->assertEquals(
-			'https:example.com:777/path/to/script.php?query=str#fragment',
+			'https:user:pass@example.com:777/path/to/script.php?query=str#fragment',
 			$uri2->replace('SCHEME_SYMBOLS', ':')
 		);
-		$uri1->reset();
-		$uri2->reset();
+		// Check Prepend
+		$this->assertEquals(
+			'://example.com',
+			$uri1->prepend('SCHEME_SYMBOLS', ':')
+		);
+		// Check Append
+		$this->assertEquals(
+			'https://user:pass@example.com:777/path/to/script.php?query=str#fragment',
+			$uri2->append('SCHEME_SYMBOLS', '//')
+		);
 	}
+	
+	/**
+	 * @test
+	 * @depends Reset
+	 * @covers uri::_modifier
+	 * @covers uri::replace
+	 * @covers uri::prepend
+	 * @covers uri::append
+	 */
+	public function Modify_Scheme() {
+		// Test both when there is and isn't pre-existing data
+		$uri1 = new uri('example.com');
+		$uri2 = new uri('https://user:pass@example.com:777/path/to/script.php?query=str#fragment');
+		
+		// Check For Errors
+		$this->assertEmpty($uri1->error);
+		$this->assertEmpty($uri2->error);
+		
+		// Check Replace
+		$this->assertEquals(
+			'//example.com',
+			$uri1->replace('SCHEME', '//')
+		);
+		$this->assertEquals(
+			'http:user:pass@example.com:777/path/to/script.php?query=str#fragment',
+			$uri2->replace('SCHEME', 'http:')
+		);
+		// Check Prepend
+		$this->assertEquals(
+			'https://example.com',
+			$uri1->prepend('SCHEME', 'https:')
+		);
+		// Check Append
+		$this->assertEquals(
+			'http://user:pass@example.com:777/path/to/script.php?query=str#fragment',
+			$uri2->append('SCHEME', '//')
+		);
+	}
+	
+	/**
+	 * @test
+	 * @depends Reset
+	 * @covers uri::_modifier
+	 * @covers uri::replace
+	 * @covers uri::prepend
+	 * @covers uri::append
+	 */
+	public function Modify_User() {
+		// Test both when there is and isn't pre-existing data
+		$uri1 = new uri('example.com');
+		$uri2 = new uri('https://user:pass@example.com:777/path/to/script.php?query=str#fragment');
+		
+		// Check For Errors
+		$this->assertEmpty($uri1->error);
+		$this->assertEmpty($uri2->error);
+		
+		// Check Replace
+		$this->assertEquals(
+			'doe@example.com',
+			$uri1->replace('USER', 'doe')
+		);
+		$this->assertEquals(
+			'https://john:pass@example.com:777/path/to/script.php?query=str#fragment',
+			$uri2->replace('USER', 'john')
+		);
+		// Check Prepend
+		$this->assertEquals(
+			'jdoe@example.com',
+			$uri1->prepend('USER', 'j')
+		);
+		// Check Append
+		$this->assertEquals(
+			'https://johnd:pass@example.com:777/path/to/script.php?query=str#fragment',
+			$uri2->append('USER', 'd')
+		);
+	}
+	
+	/**
+	 * @test
+	 * @depends Reset
+	 * @covers uri::_modifier
+	 * @covers uri::replace
+	 * @covers uri::prepend
+	 * @covers uri::append
+	 */
+	public function Modify_Pass() {
+		// Test both when there is and isn't pre-existing data
+		$uri1 = new uri('jdoe@example.com'); // MUST have a user to have a password
+		$uri2 = new uri('https://user:pass@example.com:777/path/to/script.php?query=str#fragment');
+		
+		// Check For Errors
+		$this->assertEmpty($uri1->error);
+		$this->assertEmpty($uri2->error);
+		
+		// Check Replace
+		$this->assertEquals(
+			'jdoe:1234@example.com',
+			$uri1->replace('PASS', '1234')
+		);
+		$this->assertEquals(
+			'https://user:1234@example.com:777/path/to/script.php?query=str#fragment',
+			$uri2->replace('PASS', '1234')
+		);
+		// Check Prepend
+		$this->assertEquals(
+			'jdoe:01234@example.com',
+			$uri1->prepend('PASS', '0')
+		);
+		// Check Append
+		$this->assertEquals(
+			'https://user:12345@example.com:777/path/to/script.php?query=str#fragment',
+			$uri2->append('PASS', '5')
+		);
+	}
+	
+	/**
+	 * @test
+	 * @depends Reset
+	 * @covers uri::_modifier
+	 * @covers uri::replace
+	 * @covers uri::prepend
+	 * @covers uri::append
+	 */
+	public function Modify_Host() {
+		// Test both when there is and isn't pre-existing data
+		$uri1 = new uri('example.com');
+		$uri2 = new uri('https://user:pass@example.com:777/path/to/script.php?query=str#fragment');
+		
+		// Check For Errors
+		$this->assertEmpty($uri1->error);
+		$this->assertEmpty($uri2->error);
+		
+		// Check Replace
+		$this->assertEquals(
+			'google.com',
+			$uri1->replace('HOST', 'google.com')
+		);
+		$this->assertEquals(
+			'https://user:pass@sample.co:777/path/to/script.php?query=str#fragment',
+			$uri2->replace('HOST', 'sample.co')
+		);
+		// Check Prepend
+		$this->assertEquals(
+			'www.google.com',
+			$uri1->prepend('HOST', 'www.')
+		);
+		// Check Append
+		$this->assertEquals(
+			'https://user:pass@sample.co.uk:777/path/to/script.php?query=str#fragment',
+			$uri2->append('HOST', '.uk')
+		);
+	}
+	
+	/**
+	 * @test
+	 * @depends Reset
+	 * @covers uri::_modifier
+	 * @covers uri::replace
+	 * @covers uri::prepend
+	 * @covers uri::append
+	 */
+	public function Modify_Port() {
+		// Test both when there is and isn't pre-existing data
+		$uri1 = new uri('example.com');
+		$uri2 = new uri('https://user:pass@example.com:777/path/to/script.php?query=str#fragment');
+		
+		// Check For Errors
+		$this->assertEmpty($uri1->error);
+		$this->assertEmpty($uri2->error);
+		
+		// Check Replace
+		$this->assertEquals(
+			'example.com:1234',
+			$uri1->replace('PORT', '1234')
+		);
+		$this->assertEquals(
+			'https://user:pass@example.com:1234/path/to/script.php?query=str#fragment',
+			$uri2->replace('PORT', '1234')
+		);
+		// Check Prepend
+		$this->assertEquals(
+			'example.com:01234',
+			$uri1->prepend('PORT', '0')
+		);
+		// Check Append
+		$this->assertEquals(
+			'https://user:pass@example.com:12345/path/to/script.php?query=str#fragment',
+			$uri2->append('PORT', '5')
+		);
+	}
+	
+	/**
+	 * @test
+	 * @depends Reset
+	 * @covers uri::_modifier
+	 * @covers uri::replace
+	 * @covers uri::prepend
+	 * @covers uri::append
+	 */
+	public function Modify_Path() {
+		// Test both when there is and isn't pre-existing data
+		$uri1 = new uri('example.com');
+		$uri2 = new uri('https://user:pass@example.com:777/path/to/script.php?query=str#fragment');
+		
+		// Check For Errors
+		$this->assertEmpty($uri1->error);
+		$this->assertEmpty($uri2->error);
+		
+		// Check Replace
+		$this->assertEquals(
+			'example.com/path',
+			$uri1->replace('PATH', '/path')
+		);
+		$this->assertEquals(
+			'https://user:pass@example.com:777/path?query=str#fragment',
+			$uri2->replace('PATH', '/path')
+		);
+		// Check Prepend
+		$this->assertEquals(
+			'example.com/sample/path',
+			$uri1->prepend('PATH', '/sample')
+		);
+		// Check Append
+		$this->assertEquals(
+			'https://user:pass@example.com:777/path/to/some/random/file.txt?query=str#fragment',
+			$uri2->append('PATH', '/to/some/random/file.txt')
+		);
+	}
+	
+	/**
+	 * @test
+	 * @depends Reset
+	 * @covers uri::_modifier
+	 * @covers uri::replace
+	 * @covers uri::prepend
+	 * @covers uri::append
+	 */
+	public function Modify_Query() {
+		// Test both when there is and isn't pre-existing data
+		$uri1 = new uri('example.com');
+		$uri2 = new uri('https://user:pass@example.com:777/path/to/script.php?query=str#fragment');
+		
+		// Check For Errors
+		$this->assertEmpty($uri1->error);
+		$this->assertEmpty($uri2->error);
+		
+		// Check Replace
+		$this->assertEquals(
+			'example.com',
+			$uri1->replace('PATH', '/path')
+		);
+		$this->assertEquals(
+			'https://user:pass@example.com:777/path/to/script.php?query=str#fragment',
+			$uri2->replace('PATH', '/path')
+		);
+		// Check Prepend
+		$this->assertEquals(
+			'example.com/sample/path',
+			$uri1->prepend('PATH', '/sample')
+		);
+		// Check Append
+		$this->assertEquals(
+			'https://user:pass@example.com:777/path/to/some/random/file.txt?query=str#fragment',
+			$uri2->append('PATH', '/to/some/random/file.txt')
+		);
+	}
+	
+	
 	
 	
 	
